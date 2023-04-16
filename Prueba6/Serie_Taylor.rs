7cbee1568807df6ba0214d2e2b31acd75c4bf007
Einar115 <einarnaim@hotmail.com>
2023-04-16 16:40:50 -0600

    Correcciones

--- Prueba6/Serie_Taylor.rs
+++ Prueba6/Serie_Taylor.rs
@@ -4,14 +4,13 @@ fn main(){
     let x:f32 = 0.78539; //pi/4
     let i:f32 = 8.0; //numero de iteraciones
     let n:f32 = 3.0; //N de la serie
-    let signo:f32 = -1.0; //Correccion de signo
-    let senx:f32 = taylor(x, i, n, signo); //resultado de seno de pi/4
+    let senx:f32 = taylor(x, i, n); //resultado de seno de pi/4
     println!("Seno de pi/4 es: {}", senx); //imprimir resultado
 }
 
-fn taylor(x:f32, i:f32, n:f32, signo:f32) -> f32{ //funcion de serie de taylor recursiva
+fn taylor(x:f32, i:f32, n:f32) -> f32{ //funcion de serie de taylor recursiva
     if n<i{
-        return taylor(x,i,n+2.0,signo)+((potencia(1.0,x,n)*signo)/factorial(n));
+        return taylor(x, i, n+2.0)+((potencia(1.0, x, n)*-1.0)/factorial(n));
     }
     return x;
 }
