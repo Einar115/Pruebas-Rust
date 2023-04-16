28cbcd03791cecec4f05f193aad6b8dce0332105
Einar115 <einarnaim@hotmail.com>
2023-04-16 16:57:26 -0600

    Prueba de proyectos + entrada de datos

---
+++ Nuevo_proyecto/target/debug/Nuevo_proyecto.d
@@ -0,0 +1,1 @@
+C:\Users\Usuario\.ssh\Pruebas-Rust\Pruebas-Rust\Nuevo_proyecto\target\debug\Nuevo_proyecto.exe: C:\Users\Usuario\.ssh\Pruebas-Rust\Pruebas-Rust\Nuevo_proyecto\src\main.rs
---
+++ Nuevo_proyecto/target/debug/deps/Nuevo_proyecto.d
@@ -0,0 +1,5 @@
+C:\Users\Usuario\.ssh\Pruebas-Rust\Pruebas-Rust\Nuevo_proyecto\target\debug\deps\Nuevo_proyecto.exe: src\main.rs
+
+C:\Users\Usuario\.ssh\Pruebas-Rust\Pruebas-Rust\Nuevo_proyecto\target\debug\deps\Nuevo_proyecto.d: src\main.rs
+
+src\main.rs:
