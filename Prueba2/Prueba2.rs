cd2429129e9db3a46e33ae42d6250fe7565570bc
Einar115 <einarnaim@hotmail.com>
2023-04-15 18:40:37 -0600

    Prueba 2 completada

---
+++ Prueba2/Prueba2.rs
@@ -0,0 +1,13 @@
+//perimetro y area de un circulo
+
+fn main(){
+    const PI: f32 = 3.1416;
+    let diam: f32 = 20.0;
+    let area: f32; 
+    let peri: f32;
+    peri=2.0*PI*(diam/2.0);
+    area=PI*((diam/2.0)*(diam/2.0));
+
+    println!("El perimetro del circulo es de: {}", peri);
+    println!("El area del circulo es de : {}", area);
+}
