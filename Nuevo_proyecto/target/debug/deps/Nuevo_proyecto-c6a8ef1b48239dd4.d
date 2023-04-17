2ab5f10ba7990d49423a1b6d8b73131f6ee14642
Einar115 <einarnaim@hotmail.com>
2023-04-16 19:04:51 -0600

    Detalles de debugeo

---
+++ Nuevo_proyecto/target/debug/deps/Nuevo_proyecto-c6a8ef1b48239dd4.d
@@ -0,0 +1,5 @@
+c:\Users\Usuario\.ssh\Pruebas-Rust\Pruebas-Rust\Nuevo_proyecto\target\debug\deps\Nuevo_proyecto-c6a8ef1b48239dd4.rmeta: src\main.rs
+
+c:\Users\Usuario\.ssh\Pruebas-Rust\Pruebas-Rust\Nuevo_proyecto\target\debug\deps\Nuevo_proyecto-c6a8ef1b48239dd4.d: src\main.rs
+
+src\main.rs:
---
+++ Nuevo_proyecto/target/debug/deps/Nuevo_proyecto-d654c81ca81176a9.d
@@ -0,0 +1,5 @@
+c:\Users\Usuario\.ssh\Pruebas-Rust\Pruebas-Rust\Nuevo_proyecto\target\debug\deps\Nuevo_proyecto-d654c81ca81176a9.rmeta: src\main.rs
+
+c:\Users\Usuario\.ssh\Pruebas-Rust\Pruebas-Rust\Nuevo_proyecto\target\debug\deps\Nuevo_proyecto-d654c81ca81176a9.d: src\main.rs
+
+src\main.rs:
